5cb9cc578aa5923fd60966d52318064d532c1bc9
Neha Pal <palneha1779@gmail.com>
2024-08-08 23:22:42 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,39 @@
+// If - Else
+
+let age = 19;
+let country = 'India';
+if(age>=18 && country == 'India'){
+    console.log("you can vote");
+}
+else{
+    console.log("you can not vote");
+}
+
+let day = 2;
+switch(day){
+    case 1:
+        day = 'Sunday';
+        break;
+    case 2:
+        day = 'Monday';
+        break;
+    case 3:
+        day = 'Tuesday';
+        break;
+    case 4:
+        day = 'Tuesday';
+        break;
+    case 5:
+        day = 'Tuesday';
+        break;
+    case 6:
+        day = 'Tuesday';
+        break;
+    case 7:
+        day = 'Tuesday';
+        break;
+    default:
+        day = 'Invalid';
+        break;
+}
+console.log("Day is - "+day);

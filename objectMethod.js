85002bb4d8e59df69de5bbba455425bd1332c74b
Neha Pal <palneha1779@gmail.com>
2024-08-10 08:56:47 +0530

    Add files via upload

---
+++ objectMethod.js
@@ -0,0 +1,10 @@
+const employee = {
+    name : 'Yash',
+    id : 23,
+    place : 'Kolkata',
+    welcome : function welcome(){
+        console.log("Welcome...");
+    }
+}
+
+console.log(employee); //call the method

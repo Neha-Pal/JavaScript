fb26734cf64b562d7cb7fdae01971fcdf300a3dd
Neha Pal <palneha1779@gmail.com>
2024-08-10 08:57:54 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,9 @@
+class student{
+    constructor(name,roll){
+        this.name = name;
+        this.roll = roll;
+    }
+}
+const student_1 = new student('Neha' ,56);
+const student_2 = new student('Piya',33);
+console.log(student_2);
